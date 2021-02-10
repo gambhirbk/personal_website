21e7e9154d2e118508ed4738f49d82bd5f203d58
gambhir <gambhir.b.kunwar@gmail.com>
2021-02-09 18:50:26 -0800

    finished clock

--- website/clock/main.js
+++ website/clock/main.js
@@ -1,30 +1,51 @@
-var time = new Date();
-console.log(time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"}));
-console.log(time.toLocaleTimeString("en-US", { timeZone: "America/New_York"}));
+function updateTimer() {
+    var time = new Date();
+    // document.getElementById("seattle").getElementsByClassName("hour")[0].style.transform =
+    // "translate(100px) rotate(seattle_hour*30deg)";
 
-var seattle = time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"});
-var boston = time.toLocaleTimeString("en-US", { timeZone: "America/New_York"});
+    console.log(time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"}));
+    console.log(time.toLocaleTimeString("en-US", { timeZone: "America/New_York"}));
 
-let seattle_hour = Number(seattle.split(' ')[0].split(':')[0])
-let boston_hour = Number(boston.split(' ')[0].split(':')[0])
+    var seattle = time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"});
+    var boston = time.toLocaleTimeString("en-US", { timeZone: "America/New_York"});
 
-// variable to store minutes and seconds 
-var min = time.getMinutes();
-var sec = time.getSeconds();
+    let seattle_hour = Number(seattle.split(' ')[0].split(':')[0])
+    let boston_hour = Number(boston.split(' ')[0].split(':')[0])
 
-document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
-    "translate(100px) rotate(30deg)";
+    // variable to store minutes and seconds 
+    var min = time.getMinutes();
+    var sec = time.getSeconds();
 
-document.getElementById("boston").getElementsByClassName("second") [0].style.transofrm = 
-"translate(100px) rotate(45deg)";
+    document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
+        "translate(100px) rotate(30deg)";
 
-document.getElementById("seattle").innerHTML = document.getElementById("seattle").innerHTML.replace("...", seattle.split(' ')[1]);
-document.getElementById("boston").innerHTML = document.getElementById("boston").innerHTML.replace("...", boston.split(' ')[1]);
+    document.getElementById("boston").getElementsByClassName("second") [0].style.transform = 
+    "translate(100px) rotate(45deg)";
 
+    document.getElementById("seattle").innerHTML = document.getElementById("seattle").innerHTML.replace("...", seattle.split(' ')[1]);
+    document.getElementById("boston").innerHTML = document.getElementById("boston").innerHTML.replace("...", boston.split(' ')[1]);
 
-setInterval(() => {updateSecond()
-}, 1000);
+    document.getElementById("seattle").getElementsByClassName("hour") [0].style.transform = 
+        "translate(100px) rotate(" + (seattle_hour * 30) + "deg)";
+    
+    document.getElementById("boston").getElementsByClassName("hour") [0].style.transform = 
+        "translate(100px) rotate(" + (boston_hour * 30) + "deg)";
 
-function updateSecond() {
-    document.getElementById("seattle").innerHTML
+    document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
+    "translate(100px) rotate(" + (sec * 6) + "deg)";
+
+    document.getElementById("boston").getElementsByClassName("second") [0].style.transform = 
+    "translate(100px) rotate(" + (sec * 6) + "deg)";
+
+    document.getElementById("seattle").getElementsByClassName("minute") [0].style.transform = 
+    "translate(100px) rotate(" + (min * 6) + "deg)";
+
+    document.getElementById("boston").getElementsByClassName("minute") [0].style.transform = 
+    "translate(100px) rotate(" + (min * 6) + "deg)";
+
 }
+
+// setInterval(() => {updateTimer()
+// }, 1000);
+
+setInterval(updateTimer, 1000);
