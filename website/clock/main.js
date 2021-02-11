dcbe72fa07bf36deb4ea6afc2d07f37dd5e788ac
gambhir <gambhir.b.kunwar@gmail.com>
2021-02-10 21:44:01 -0800

    completed the initial phase of project

--- website/clock/main.js
+++ website/clock/main.js
@@ -1,10 +1,9 @@
+const HOUR_DEGREE = 30;
+const MIN_SEC_DEGREE = 6;
+const MILLISECOND = 1000;
+
 function updateTimer() {
     var time = new Date();
-    // document.getElementById("seattle").getElementsByClassName("hour")[0].style.transform =
-    // "translate(100px) rotate(seattle_hour*30deg)";
-
-    console.log(time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"}));
-    console.log(time.toLocaleTimeString("en-US", { timeZone: "America/New_York"}));
 
     var seattle = time.toLocaleTimeString("en-US", { timeZone: "America/Los_Angeles"});
     var boston = time.toLocaleTimeString("en-US", { timeZone: "America/New_York"});
@@ -16,36 +15,29 @@ function updateTimer() {
     var min = time.getMinutes();
     var sec = time.getSeconds();
 
-    document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
-        "translate(100px) rotate(30deg)";
-
-    document.getElementById("boston").getElementsByClassName("second") [0].style.transform = 
-    "translate(100px) rotate(45deg)";
-
     document.getElementById("seattle").innerHTML = document.getElementById("seattle").innerHTML.replace("...", seattle.split(' ')[1]);
     document.getElementById("boston").innerHTML = document.getElementById("boston").innerHTML.replace("...", boston.split(' ')[1]);
 
     document.getElementById("seattle").getElementsByClassName("hour") [0].style.transform = 
-        "translate(100px) rotate(" + (seattle_hour * 30) + "deg)";
+        "translate(100px) rotate(" + (seattle_hour * HOUR_DEGREE) + "deg)";
     
     document.getElementById("boston").getElementsByClassName("hour") [0].style.transform = 
-        "translate(100px) rotate(" + (boston_hour * 30) + "deg)";
-
-    document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
-    "translate(100px) rotate(" + (sec * 6) + "deg)";
-
-    document.getElementById("boston").getElementsByClassName("second") [0].style.transform = 
-    "translate(100px) rotate(" + (sec * 6) + "deg)";
+        "translate(100px) rotate(" + (boston_hour * HOUR_DEGREE) + "deg)";
 
     document.getElementById("seattle").getElementsByClassName("minute") [0].style.transform = 
-    "translate(100px) rotate(" + (min * 6) + "deg)";
+    "translate(100px) rotate(" + (min * MIN_SEC_DEGREE) + "deg)";
 
     document.getElementById("boston").getElementsByClassName("minute") [0].style.transform = 
-    "translate(100px) rotate(" + (min * 6) + "deg)";
+    "translate(100px) rotate(" + (min * MIN_SEC_DEGREE) + "deg)";
+
+    document.getElementById("seattle").getElementsByClassName("second") [0].style.transform = 
+    "translate(100px) rotate(" + (sec * MIN_SEC_DEGREE) + "deg)";
+
+    document.getElementById("boston").getElementsByClassName("second") [0].style.transform = 
+    "translate(100px) rotate(" + (sec * MIN_SEC_DEGREE) + "deg)";
 
 }
 
-// setInterval(() => {updateTimer()
-// }, 1000);
+setInterval(() => {updateTimer()
+}, MILLISECOND);
 
-setInterval(updateTimer, 1000);
